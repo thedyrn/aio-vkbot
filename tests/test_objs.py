991c3613daea71dcb0769080d0f982148ad31426
thedyrn <knave1997@gmail.com>
2020-01-29 22:11:36 +0300

    tdd tests for FSM, new fixtures in confest.py

--- tests/test_objs.py
+++ tests/test_objs.py
@@ -3,8 +3,8 @@ import pytest
 
 
 @pytest.fixture
-def msg_obj(new_message_update):
-    message = new_message_update['object']['message']
+def msg_obj(raw_new_message_update):
+    message = raw_new_message_update['object']['message']
     msg_obj = Message(date=message['date'], from_id=message['from_id'], msg_id=message['id'],
                       peer_id=message['peer_id'], text=message['text'],
                       attachments=message['attachments'], fwd_messages=message['fwd_messages'],
@@ -12,13 +12,13 @@ def msg_obj(new_message_update):
     return msg_obj
 
 
-def test_message_from_dict(new_message_update, msg_obj):
-    msg_from_dict = Message.from_dict(new_message_update['object']['message'])
+def test_message_from_dict(raw_new_message_update, msg_obj):
+    msg_from_dict = Message.from_dict(raw_new_message_update['object']['message'])
     assert msg_from_dict == msg_obj
 
 
-def test_update_from_dict(new_message_update, msg_obj):
-    upd = Update.from_dict(new_message_update)
+def test_update_from_dict(raw_new_message_update, msg_obj):
+    upd = Update.from_dict(raw_new_message_update)
     assert upd.message == msg_obj
     assert upd.type == VkEventType.MESSAGE_NEW
     assert isinstance(upd, Update)
