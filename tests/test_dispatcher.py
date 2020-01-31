1d82ea8fd8250b16bd4ba101d9e6cd18a11e322f
thedyrn <knave1997@gmail.com>
2020-02-01 01:59:38 +0300

    test for Dispatcher.close()

--- tests/test_dispatcher.py
+++ tests/test_dispatcher.py
@@ -24,3 +24,22 @@ async def test_dispatcher(queue_with_event):
     await asyncio.sleep(0.5)  # дать времени диспетчеру отработать
     assert mocked_callback.called
     task.cancel()
+
+
+@pytest.mark.asyncio
+async def test_dispatcher_close(queue_with_event):
+    done_cb_called = False
+
+    def done_callback(done_task):
+        nonlocal done_cb_called
+        print(f' Close Dispatcher: {done_task.__repr__()[1:14]}...')
+        done_cb_called = True
+
+    mocked_bot = Mock()
+    dp = Dispatcher(mocked_bot)
+    task = asyncio.create_task(dp.start(queue_with_event))
+    task.add_done_callback(done_callback)
+    dp.close()
+    assert dp._closed
+    await asyncio.sleep(0.5)
+    assert done_cb_called
